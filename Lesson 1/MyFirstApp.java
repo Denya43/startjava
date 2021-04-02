b013fd68fcef7aaffc488e1e50bed78be8339ff7
Denis <gdenia1@gmail.com>
2021-04-03 00:55:52 +0300

    add my first app

---
+++ Lesson 1/MyFirstApp.java
@@ -0,0 +1,9 @@
+public class MyFirstApp {
+    public static void main(String[] args) {
+
+        for(int i = 33; i <= 126; i++){
+            char tempChar = (char)i;
+            System.out.println(tempChar);
+        }
+    }
+}
