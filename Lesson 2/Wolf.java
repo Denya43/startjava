549afdba422f77bb8d21a7a4f28eff8fdf0cd2f3
Denis <gdenia1@gmail.com>
2021-04-07 14:44:26 +0300

    fix wolf

--- Lesson 2/Wolf.java
+++ Lesson 2/Wolf.java
@@ -1,81 +1,82 @@
 public class Wolf {	
 	
 	private String sex;
-	private String nick;
-	private String color;
-	private int height;
-	private int weight;
-	private int age;
+    private String nickname;
+    private String color;
+    private int height;
+    private int weight;
+    private int age;
 
-	public void walk() {
-		System.out.println("Ходить");
-	}
+    public void walk() {
+        System.out.println("Ходить");
+    }
 
-	public void sit() {
-		System.out.println("Сидеть");
-	}
+    public void sit() {
+        System.out.println("Сидеть");
+    }
 
-	public void run() {
-		System.out.println("Бежать");
-	}
+    public void run() {
+        System.out.println("Бежать");
+    }
 
-	public void howl() {
-		System.out.println("Воет");
-	}
+    public void howl() {
+        System.out.println("Воет");
+    }
 
-	public void hunt() {
-		System.out.println("Охотится");
-	}
+    public void hunt() {
+        System.out.println("Охотится");
+    }
 
-	public void setAge (int age) {
-		if (age > 0 & age <= 8) {
-			this.age = age;
-		} else {
-			System.out.println("Некорректный возраст");
-		}
-	}
+    public int getAge() {
+        return age;
+    }
+    
+    public void setAge(int age) {
+        if (age > 0 & age <= 8) {
+            this.age = age;
+        } else {
+            System.out.println("Некорректный возраст");
+        }
+    }
+    
+    public String getSex() {
+        return sex;
+    }
 
-	public void setNick (String nick) {
-		this.nick = nick;
-	}
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
 
-	public void setColor (String color) {
-		this.color = color;
-	}
+    public String getNickname() {
+        return nickname;
+    }
 
-	public void setSex (String sex) {
-		this.sex = sex;
-	}
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
 
-	public void setHeight (int height) {
-		this.height = height;
-	}
+    public String getColor() {
+        return color;
+    }
 
-	public void setWeight (int weight) {
-		this.weight = weight;
-	}
+    public void setColor(String color) {
+        this.color = color;
+    }
 
-	public int getAge () {
-		return age;
-	}
+    public int getHeight() {
+        return height;
+    }
 
-	public String getNick () {
-		return nick;
-	}
+    public void setHeight(int height) {
+        this.height = height;
+    }
 
-	public String getColor () {
-		return color;
-	}
+    public int getWeight() {
+        return weight;
+    }
 
-	public String getSex () {
-		return sex;
-	}
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
 
-	public int getHeight () {
-		return height;
-	}
-
-	public int getWeight () {
-		return weight;
-	}
 }
